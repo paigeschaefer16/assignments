61db212752e9d8625f473699ff7b1727df8598ed
paigeschaefer16 <paigeschaefer16@gmail.com>
2022-01-20 21:01:05 -0500

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,14 @@
+/***************************************************
+ * fortune.c
+ * Author: Paige Schaefer
+ * Date: 1/20/22
+ * Print Fortune
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("You will own a flower shop\n");
+  return 0;
+}
+
