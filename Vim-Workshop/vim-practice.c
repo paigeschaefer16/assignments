387a6cf089ad594c307ad72728123c6980b791e6
paigeschaefer16 <paigeschaefer16@gmail.com>
2022-01-27 17:37:58 -0500

    vim workshop

--- Vim-Workshop/vim-practice.c
+++ Vim-Workshop/vim-practice.c
@@ -1,20 +1,25 @@
 /*
- * Put your name and motto here
+ * Paige Schaefer, I don't love Vim.  
  */
-include <stdio.h>
+#include <stdio.h>
 
 int main() {
-printf("Hello vim")
-int a = 0
-scanf("Enter a number: ");
-if (a < 0) {
-  print("a less than zero");
-} else if (a > 0) {
-  print("a greater than zero");
-} else {
-  print(you win!)
-}
-  
+  printf("Hello vim");
+  int a = 0;
+  scanf("Enter a number: ");
+  if (a < 0) {
+    print("a less than zero");
+  } else if (a > 0) {
+    print("a greater than zero");
+  } else {
+    print(you win!);
+  }
+  int arr[5];
+  arr[0] = 99;
+  arr[1] = 99;
+  arr[2] = 99;
+  arr[3] = 99;
+  arr[4] = 99;
 }
 }
 
